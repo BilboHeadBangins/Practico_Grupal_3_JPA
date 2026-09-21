4aa4f5d272f7c62f21895951be2e6469507425b4
BilboHeadBangins <italoleonardo24345@gmail.com>
2026-09-21 01:28:26 -0300

    fin nivel 2

--- src/main/java/com/mycompany/practico_grupal_2_jpa/entities/FacturaVenta.java
+++ src/main/java/com/mycompany/practico_grupal_2_jpa/entities/FacturaVenta.java
@@ -2,9 +2,7 @@
 package com.mycompany.practico_grupal_2_jpa.entities;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
